8df87051168869bb6d78283f31711f6d0506ea6f
Andi <trynadizanuar@gmail.com>
2026-05-15 12:41:39 +0700

    update latihan for loop

--- projects/experimental/challenge/LATIHAN_for/js/test.js
+++ projects/experimental/challenge/LATIHAN_for/js/test.js
@@ -1,10 +1,42 @@
-let baris = 15;
-// looping baris 
-for (let i = 5; i <= 15; i++) {
-    let pola = "";
+// ===== DATABASE =====
+const users = [
+    { id: 1, name: 'Andi', age: 20 },
+    { id: 2, name: 'Budi', age: 22 },
+    { id: 3, name: 'Caca', age: 19 }
+]
 
-    // looping bintang 
-    for (let j = 5; j <= i; j++)
-        pola += "*";
-    console.log(pola);
-}
+
+// ===== CREATE =====
+users.push({ id: 4, name: 'Doni', age: 25 })
+
+console.log('=== CREATE ===')
+console.log(users)
+
+
+// ===== READ =====
+console.log('\n=== READ ===')
+
+users.forEach((item) => {
+    console.log(`${item.id} - ${item.name} - ${item.age}`)
+})
+
+
+// ===== UPDATE =====
+users.forEach((item) => {
+    if (item.id === 2) {
+        item.name = 'Budi Santoso'
+        item.age = 24
+    }
+})
+
+console.log('\n=== UPDATE ===')
+console.log(users)
+
+
+// ===== DELETE =====
+const hasil = users.filter((item) => {
+    return item.id !== 2
+})
+
+console.log('\n=== DELETE ===')
+console.log(hasil)
