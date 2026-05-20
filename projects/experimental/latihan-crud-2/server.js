d1cf7f7338653c4a88c00dfed6ac0f80696faecc
Andi <trynadizanuar@gmail.com>
2026-05-20 21:44:15 +0700

    Implement update and delete feature for CRUD app

--- projects/experimental/latihan-crud-2/server.js
+++ projects/experimental/latihan-crud-2/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { Pool } = require('pg');
 const app = express()
+app.use(express.urlencoded({ extended: true }))
 app.set("view engine", "ejs");
 app.use(express.static("public"))
 
@@ -30,21 +31,61 @@ app.get('/create', (req, res) => {
 
 app.post('/create', (req, res) => {
 
-    app.post('/add', (req, res) => {
-        db.query(
-            "INSERT INTO users (name, height, weight, birth_date, married) VALUES ($1,$2,$3,$4,$5)",
-            [req.body.name, req.body.height, req.body.weight, req.body.birth_date, req.body.married === '1'],
-            (err) => {
-                if (err) {
-                    console.log(err)
-                    return res.send("Gagal insert data")
-                }
-                res.redirect('/')
+    db.query(
+        "INSERT INTO users (name, height, weight, birth_date, is_married) VALUES ($1,$2,$3,$4,$5)",
+        [req.body.name, req.body.height, req.body.weight, req.body.birth_date, req.body.married === '1'],
+        (err) => {
+            if (err) {
+                console.log(err)
+                return res.send("Gagal insert data")
             }
-        )
-    })
-
+            res.redirect('/')
+        }
+    )
 })
+
+app.get('/update/:id', (req, res) => {
+    db.query(
+        "SELECT * FROM users WHERE id=$1",
+        [req.params.id],
+        (err, result) => {
+            if (err) return res.send('error')
+
+            const item = result.rows[0]
+
+            if (item.birth_date) {
+                item.birth_date = item.birth_date.toISOString().split('T')[0]
+            }
+
+            res.render('add', { item })
+        }
+    )
+})
+
+app.post('/update/:id', (req, res) => {
+    db.query(
+        "UPDATE users SET name=$1, height=$2, weight=$3, birth_date=$4, is_married=$5 WHERE id=$6",
+        [req.body.name, req.body.height, req.body.weight, req.body.birth_date, req.body.is_married === '1', req.params.id],
+        (err) => {
+            if (err) {
+                console.log(err)
+                return res.send('Gagal update')
+            }
+
+            res.redirect('/')
+        }
+    )
+})
+
+
+app.get('/delete/:id', (req, res) => {
+    db.query("DELETE FROM users WHERE id=$1", [req.params.id], (err) => {
+        if (err) return console.log(err)
+        res.redirect('/')
+
+    })
+})
+
 app.listen(3000, () => {
     console.log('Server jalan di http://localhost:3000')
 })
